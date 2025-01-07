a8beca00d2a6b583dbab5602b3fe820083bfdf71
Popescu Daniel <same.dan@gmail.com>
2025-01-07 07:22:00 +0100

    Created User on DBB

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,22 @@
-export const register = async () => {};
+import bcrypt from "bcryptjs";
+import TwoFaUser from "../models/user.js";
+
+export const register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new TwoFaUser({
+      username,
+      password: hashedPassword,
+      isMfaActive: false,
+    });
+    console.log("New User: ", newUser);
+    await newUser.save();
+    res.status(201).json({ message: "User registered" });
+  } catch (error) {
+    res.status(500).json({ error: "Errore registering user", message: error });
+  }
+};
 export const login = async () => {};
 export const authStatus = async () => {};
 export const logout = async () => {};
