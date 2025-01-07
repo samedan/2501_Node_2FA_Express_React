a80ee0eaa67f50974168a2a32cacffe0f381d6c3
Popescu Daniel <same.dan@gmail.com>
2025-01-07 06:44:47 +0100

    Revert "Connected DBB, CorsOptions"
    This reverts commit cde302d122f214c1e5d669f14d64787399f47946.

--- Readme.md
+++ Readme.md
@@ -1,14 +1,3 @@
 # ES6
 
 > package.json -> "type":"module"
-
-### Origin CORS
-
-> /src/index.js
-
-```
-const corsOptions = {
-  origin: ["http://localhost:3001"],
-  credentials: true,
-};
-```
--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ import session from "express-session";
 import passport from "passport";
 import dotenv from "dotenv";
 import cors from "cors";
-import dbConnect from "./config/dbConnect.js";
 
 dotenv.config("");
-
-//DBB
-dbConnect();
 
 const app = express();
 
