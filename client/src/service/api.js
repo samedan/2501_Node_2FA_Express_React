8ec07b04215363e64f89f83aaedca968a7c89d00
John Doe <johndoe@example.com>
2025-02-01 20:17:02 +0100

    Home server IP

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://localhost:7001/api`,
+  // baseURL: `http://localhost:7001/api`,
+  baseURL: `http://176.169.113.147:90/api`,
   // baseURL: `https://two501-node-2fa-express-react.onrender.com/api`,
 });
